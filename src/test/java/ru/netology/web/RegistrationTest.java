a0f2563993fee2686062dca7abca36cb9a25ba0f
A. Stefanovich <stefanovich.anastasiya@yandex.ru>
2023-05-15 02:26:42 +0300

    final result

--- src/test/java/ru/netology/web/RegistrationTest.java
+++ src/test/java/ru/netology/web/RegistrationTest.java
@@ -1,34 +1,31 @@
 package ru.netology.web;
 
+import com.codeborne.selenide.SetValueOptions;
 import org.junit.jupiter.api.Test;
 
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 class RegistrationTest {
-    @Test
-    void shouldRegisterByAccountNumberDOMModification() {
-        open("http://localhost:9999");
-        $$(".tab-item").find(exactText("По номеру счёта")).click();
-        $("[name='number']").setValue("4055 0100 0123 4613 8564");
-        $("[name='phone']").setValue("+792000000000");
-        $$("button").find(exactText("Продолжить")).click();
-        $(withText("Успешная авторизация")).shouldBe(visible, Duration.ofMillis(5000));
-        $(byText("Личный кабинет")).shouldBe(visible, Duration.ofMillis(5000));
-    }
 
     @Test
-    void shouldRegisterByAccountNumberVisibilityChange() {
+    void shouldBook() {
         open("http://localhost:9999");
-        $$(".tab-item").find(exactText("По номеру счёта")).click();
-        $$("[name='number']").last().setValue("4055 0100 0123 4613 8564");
-        $$("[name='phone']").last().setValue("+792000000000");
-        $$("button").find(exactText("Продолжить")).click();
-        $(withText("Успешная авторизация")).shouldBe(visible, Duration.ofSeconds(5));
-        $(byText("Личный кабинет")).shouldBe(visible, Duration.ofSeconds(5));
+        $("[data-test-id=city] input").setValue("Владимир");
+        LocalDate date = LocalDate.now();
+        $("[data-test-id=date] input").setValue(String.valueOf(date.plusDays(3)));
+        $("[data-test-id=name] input").setValue("Сергеев Андрей");
+        $("[data-test-id=phone] input").setValue("+79005888885");
+        $("[data-test-id=agreement]").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id=notification]").shouldBe(visible, Duration.ofSeconds(15));
+        $x("//*[contains(text(), 'Встреча успешно забронирована')]").shouldBe(visible);
     }
 }
 
